e851c11885f9088ade38279cbf1daaa47b2440da
marcosddp <87509526+marcosddp@users.noreply.github.com>
2022-06-27 15:45:24 -0300

    hambuger y hero

--- script/hambuger.js
+++ script/hambuger.js
@@ -1,22 +1,45 @@
 const navMobile = document.getElementById("header__nav-mobile");
-const fog=document.getElementById("fog");
+const fog = document.getElementById("fog");
 const hamburgerButton = document.getElementById("header__hamburger");
-console.log(navMobile);
+const iHamburger = hamburgerButton.querySelectorAll("i");
+
 hamburgerButton.addEventListener("click", () => {
- console.log(navMobile);
- navMobile.classList.toggle("hidden");
- fog.classList.toggle("hidden");
+  navMobile.classList.toggle("hidden");
+  fog.classList.toggle("hidden");
+  hamburgerButton.classList.toggle("menu-open");
+  hamburgerButton.classList.toggle("menu-close");
 });
 
-console.log(window.innerWidth)
+fog.addEventListener("click", () => {
+  navMobile.classList.toggle("hidden");
+  fog.classList.toggle("hidden");
+  hamburgerButton.classList.toggle("menu-open");
+  hamburgerButton.classList.toggle("menu-close");
+});
 
 function only768(x) {
-    if (x.matches) { // If media query matches
-      navMobile.classList.add("hidden") 
-      fog.classList.add("hidden")
-    }
+  if (x.matches) {
+    navMobile.classList.add("hidden");
+    fog.classList.add("hidden");
   }
+}
+/*
+function animationhamburger() {
+  const animacion = hamburgerButton.animate(
+    [{ transform: "TranslateY(50%)" }],
+    {
+      easing: "linear",
+      iterations: 10,
+      duration: 2000,
+    }
+  );
 
-var listener768 = window.matchMedia("(min-width: 768px)")
-only768(listener768 ) // Call listener function at run time
-listener768.addListener(only768) // Attach listener function on state changes
+  return animacion.finished;
+}
+
+
+animationhamburger()*/
+
+var listener768 = window.matchMedia("(min-width: 768px)");
+only768(listener768);
+listener768.addListener(only768);
